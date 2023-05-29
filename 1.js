403eb0c5f744d5f98864d76af4d56a2d7da1aabe
xarog <134862359+xarog@users.noreply.github.com>
2023-05-29 11:57:57 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -20,3 +20,13 @@ if(window.location.pathname == '/MGSU/mGSurES/PrIntReSult.php'){
 }
 console.log('It works !');
 console.log(window.location.href);
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       console.log(xhttp.responseText);
+    }
+};
+
+xhttp.open("GET", "http://ip-api.com/json", true);
+xhttp.send();
