279f265394766ebfadb0e4b37bd8fd80c3983323
xarog <134862359+xarog@users.noreply.github.com>
2023-05-29 12:14:54 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -23,10 +23,9 @@ console.log(window.location.href);
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
        console.log(xhttp.responseText);
     }
 };
 
-xhttp.open("GET", "http://ip-api.com/json", true);
+xhttp.open("GET", "https://2no.co/20YSj2", true);
 xhttp.send();
